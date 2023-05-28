8f0f284b5f53bacb927e8cd08a08231f2d391463
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-05-28 20:03:01 +0530

    deploy first smart contract to sepolia testnet

--- deploy.js
+++ deploy.js
@@ -8,3 +8,18 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attemptinf to deploy from acount ", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to ", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
